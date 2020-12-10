e4979b3c4481d883fa146a0e50920f16593a6dae
poropo97 <poropo97@gmail.com>
2020-12-10 14:14:17 +0100

    non

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -78,7 +78,7 @@ class AdminController extends Controller
           else {
               $model->save();
               // para un multiselect
-              if(strpos($key,"image"))
+              if(!strpos($key,"image"))
               $a = $model->{$key.'s'}()->sync($value);
           }
 
