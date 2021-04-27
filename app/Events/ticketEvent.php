57b51c4d3a805bcdcdc246523263e5224b47d906
poropo97 <Ganimedes97>
2021-04-27 16:06:49 +0200

    d

--- app/Events/ticketEvent.php
+++ app/Events/ticketEvent.php
@@ -26,7 +26,7 @@ class ticketEvent implements ShouldBroadcastNow
     public function __construct(Purchase $purchaseIn)
     {
         //
-        $this->purchase = $purchaseIn;
+        $this->purchase = Purchase::find($purchaseIn->id)->with('user');
         $this->app_id = env('APP_ID', '97');
     }
 
